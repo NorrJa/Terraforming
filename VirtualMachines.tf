93510c2237c16ff3e653d19d60c6f05694902fef
Norris <jason.norris@cgi.com>
2025-09-09 13:42:43 -0400

    adding second VM for labbing

--- VirtualMachines.tf
+++ VirtualMachines.tf
@@ -31,3 +31,37 @@ resource "azurerm_virtual_machine" "TestVM" {
         disable_password_authentication = false
     }
 }
+
+resource "azurerm_virtual_machine" "TestVM2" {
+    name                  = "TestVM2"
+    location              = azurerm_resource_group.TestRG.location
+    resource_group_name   = azurerm_resource_group.TestRG.name
+    network_interface_ids = [azurerm_network_interface.TestNIC2.id]
+    vm_size               = var.vm_size
+    delete_os_disk_on_termination = true
+    delete_data_disks_on_termination = true
+
+    storage_os_disk {
+        name              = "TestOSDisk2"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "TestVM2"
+        admin_username = var.admin_username
+        admin_password = var.admin_password
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+}
--- main.tf
+++ main.tf
@@ -27,3 +27,15 @@ resource "azurerm_network_interface" "TestNIC" {
         private_ip_address_allocation = "Dynamic"
     }
 }
+
+resource "azurerm_network_interface" "TestNIC2" {
+    name                = "TestNIC2"
+    location            = azurerm_resource_group.TestRG.location
+    resource_group_name = azurerm_resource_group.TestRG.name
+
+    ip_configuration {
+        name                          = "internal"
+        subnet_id                     = azurerm_subnet.TestSubnet.id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
